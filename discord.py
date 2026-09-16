a3e08b7615b53fb859b7f28c0bb4ef51581d9bdc
dalew14 <dale37373@gmail.com>
2026-09-16 13:43:53 -0400

    Created discord.py W/ instructions
    The plan and what we're gonna do

---
+++ discord.py
@@ -0,0 +1,6 @@
+#Get an openAI api key, Ik where to get it
+#Then pip install open ai in the terminal
+#then code the ai in here to be called on by keyword
+#then make a discord bot on this discord dev page
+#success!
+#any questions?
