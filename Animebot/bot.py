09692dc7c74e6937c9fa127dad37d56486c61636
Keon White <whitekj6@etsu.edu>
2026-09-18 15:21:44 -0400

    Discord Bot Creation,
    Added the virtual environment, and connected the discord bot token and the open ai token as well

--- Animebot/bot.py
+++ Animebot/bot.py
@@ -1,10 +1,3 @@
-#Get an openAI api key, Ik where to get it
-#Then pip install open ai in the terminal
-#then code the ai in here to be called on by keyword
-#then make a discord bot on this discord dev page
-#success!
-#any questions?
-
 import os
 
 import discord
@@ -27,12 +20,12 @@ bot = commands.Bot(
 @bot.event
 async def on_ready():
     print(f"Logged in as {bot.user}")
-    print("AnimeOracle is online!")
+    print("YapBot is online!")
 
 
 @bot.command()
 async def hello(ctx):
-    await ctx.send("Hello! I'm AnimeOracle!")
+    await ctx.send("Hello! I'm YapBot!")
 
 
 if not TOKEN:
