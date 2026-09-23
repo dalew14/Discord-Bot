b52734236f988a5b3fcccb096da333957ae66c32
dalew14 <dale37373@gmail.com>
2026-09-23 13:00:22 -0400

    Removing AI, Updating stuff with help
    Had to use a bit of the ol chat, youtube tutorial wasnt really helping me out with what I needed

--- Animebot/bot.py
+++ Animebot/bot.py
@@ -17,7 +17,14 @@ ai_client = AsyncOpenAI(api_key=OPENAI_API_KEY)
 intents = discord.Intents.default()
 intents.message_content = True
 
-crole = "ChainsawMan"
+
+anime_roles = {
+    "chainsawman": "ChainsawMan",
+    "jjk": "JJK",
+    "gachiakuta": "Gachiakuta",
+    "naruto": "Naruto",
+    "bleach": "Bleach"
+}
 
 bot = commands.Bot(
     command_prefix="!",
@@ -39,13 +46,83 @@ async def hello(ctx):
 async def paynis(ctx):
     await ctx.send("paynis")
 
+
 @bot.command()
-async def assign(ctx):
-    role = discord.utils.get(ctx.guild.roles, name=crole)
-    if role:
-        await ctx.author.add_roles(role)
-        await ctx.send(f"{ctx.author.mention} has been assigned the role '{crole}'!")
-    else:
-        await ctx.send(f"Role Doesn't Exist")
-    
+async def assign(ctx, anime=None):
+
+    if anime is None:
+        await ctx.send(
+            "Usage: `!assign <anime>`\n"
+            "Available: chainsawman, jjk, onepiece, naruto, aot"
+        )
+        return
+
+    anime = anime.lower()
+
+    if anime not in anime_roles:
+        await ctx.send("That anime role doesn't exist.")
+        return
+
+    role_name = anime_roles[anime]
+
+    role = discord.utils.get(ctx.guild.roles, name=role_name)
+
+    if role is None:
+        await ctx.send(f"The role `{role_name}` doesn't exist.")
+        return
+
+    await ctx.author.add_roles(role)
+
+    await ctx.send(
+        f"{ctx.author.mention} has been given the `{role_name}` role!"
+    )
+
+@bot.command()
+async def remove(ctx, anime=None):
+
+    if anime is None:
+        await ctx.send(
+            "Usage: `!remove <anime>`\n"
+            "Example: `!remove jjk`"
+        )
+        return
+
+    anime = anime.lower()
+
+    if anime not in anime_roles:
+        await ctx.send("That anime role doesn't exist.")
+        return
+
+    role_name = anime_roles[anime]
+
+    role = discord.utils.get(ctx.guild.roles, name=role_name)
+
+    if role is None:
+        await ctx.send(f"The role `{role_name}` doesn't exist.")
+        return
+
+    if role not in ctx.author.roles:
+        await ctx.send(f"You don't have the `{role_name}` role.")
+        return
+
+    await ctx.author.remove_roles(role)
+
+    await ctx.send(
+        f"{ctx.author.mention} has had the `{role_name}` role removed."
+    )
+
+@bot.command()
+async def roles(ctx):
+
+    available_roles = "\n".join(
+        f"`{key}` → {value}"
+        for key, value in anime_roles.items()
+    )
+
+    await ctx.send(
+        "**Anime Roles**\n\n"
+        f"{available_roles}\n\n"
+        "Use `!assign <anime>` to get a role."
+    )
+
 bot.run(DISCORD_TOKEN)
